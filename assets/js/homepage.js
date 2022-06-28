0f63c7ba522c5cd6a02e4e882e9b7a9a8eaca76b
Alex <lex.courtney18@gmail.com>
2022-06-28 09:32:57 -0500

    update js to match snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,12 +4,12 @@ var getUserRepos = function(user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {  
-        console.log(data);
+        console.log(response);
+        response.json().then(function(data) {  
+            console.log(data);
       });
-});
+    });
 };
 
-console.log("outside");
 
 getUserRepos();
